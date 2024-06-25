0382d36c95e1e2ca216e0490132a3c7d68b30a5c
matheusserafimm <matheus.serafim.t_mentores@grupoboticario.com.br>
2024-06-25 18:21:42 -0300

    fiz: prettier config

--- src/app/_components/ui/skeleton.tsx
+++ src/app/_components/ui/skeleton.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/app/_lib/utils"
+import { cn } from '@/app/_lib/utils'
 
 function Skeleton({
   className,
@@ -6,7 +6,7 @@ function Skeleton({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      className={cn('animate-pulse rounded-md bg-primary/10', className)}
       {...props}
     />
   )
--- src/app/_components/ui/toaster.tsx
+++ src/app/_components/ui/toaster.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import {
   Toast,
@@ -7,8 +7,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/app/_components/ui/toast"
-import { useToast } from "@/app/_components/ui/use-toast"
+} from '@/app/_components/ui/toast'
+import { useToast } from '@/app/_components/ui/use-toast'
 
 export function Toaster() {
   const { toasts } = useToast()
